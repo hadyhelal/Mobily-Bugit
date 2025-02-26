8f374819575df17958d4f90d9f769ec91272865a
Hady Helal <>
2025-02-26 20:53:30 +0200

    initial commit with .gitignore

---
+++ Mobily BugIt/Mobily_BugItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mobily_BugItApp.swift
+//  Mobily BugIt
+//
+//  Created by Hady Helal on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Mobily_BugItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
