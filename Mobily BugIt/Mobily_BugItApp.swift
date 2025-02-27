f3763026ba3e7fbc782e2805bac7ccc418af6cc9
Hady Helal <>
2025-02-27 19:44:40 +0200

    configure firebase

--- Mobily BugIt/Mobily_BugItApp.swift
+++ Mobily BugIt/Mobily_BugItApp.swift
@@ -6,12 +6,40 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import GoogleSignIn
 
 @main
-struct Mobily_BugItApp: App {
+struct BugItApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        let handled = GIDSignIn.sharedInstance.handle(url)
+        if handled { return true }
+        return false
+    }
+    
+    func application(_ app: UIApplication, open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
+        let handled = GIDSignIn.sharedInstance.handle(url)
+        if handled { return true }
+        return false
     }
 }
